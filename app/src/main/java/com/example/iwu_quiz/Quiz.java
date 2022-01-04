aed274f9fb585f11937bcbccb1c4c20cc84de449
JeongHun <52409864+jeonghun98@users.noreply.github.com>
2022-01-04 18:37:05 +0900

    quiz_random list를 위한 함수 변경

--- app/src/main/java/com/example/iwu_quiz/Quiz.java
+++ app/src/main/java/com/example/iwu_quiz/Quiz.java
@@ -8,23 +8,62 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class Quiz extends AppCompatActivity { // 퀴즈
+    int rand = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        Button button1 = findViewById(R.id.B1);
+        Button button2 = findViewById(R.id.B2);
+        Button button3 = findViewById(R.id.B3);
+        Button button4 = findViewById(R.id.B4);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(rand == 1) showMessage_quiz(1); // 정답 -> 1
+                else showMessage_quiz(0); // 오답 -> 0
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(rand == 2) showMessage_quiz(1); // 정답 -> 1
+                else showMessage_quiz(0); // 오답 -> 0
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(rand == 3) showMessage_quiz(1); // 정답 -> 1
+                else showMessage_quiz(0); // 오답 -> 0
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(rand == 4) showMessage_quiz(1); // 정답 -> 1
+                else showMessage_quiz(0); // 오답 -> 0
+            }
+        });
     }
 
-    public void Quiz1_ButtonClicked(View v) { //정답 -> 1
-        showMessage_quiz(1);
-    }
-    public void Quiz0_ButtonClicked(View v) { //오답 -> 0
-        showMessage_quiz(0);
-    }
+//    public void Quiz1_ButtonClicked(View v) { //정답 -> 1
+//        showMessage_quiz(1);
+//    }
+//    public void Quiz0_ButtonClicked(View v) { //오답 -> 0
+//        showMessage_quiz(0);
+//    }
 
     private void showMessage_quiz(int key) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
